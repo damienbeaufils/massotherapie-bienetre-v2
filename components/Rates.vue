42d43483237a173cea2f0c45a6d6eac3017331eb
Damien Beaufils <damien.beaufils@gmail.com>
2022-04-11 07:51:35 -0400

    💬 remove clinique physio thera plus

--- components/Rates.vue
+++ components/Rates.vue
@@ -56,64 +56,6 @@
           </iframe>
         </v-col>
       </v-row>
-
-      <v-row>
-        <v-col cols="12" sm="6">
-          <v-card class="custom-color-light-grey mt-10">
-            <v-card-title
-              class="justify-center custom-color-green mx-5 mx-sm-15 white--text font-weight-bold card-header rounded py-2"
-            >
-              Mardi, Mercredi & Vendredi
-            </v-card-title>
-
-            <v-card-text class="custom-color-green--text justify-center">
-              <p class="font-weight-bold">
-                Clinique Physio Théra Plus - Villeray
-              </p>
-              <address>
-                <p>8290 R. Saint-Denis #2, Montréal, QC H2P 2G6</p>
-                <p>
-                  <img
-                    src="/img/Montreal_Metro.svg"
-                    alt=""
-                    class="metro-logo"
-                  />&nbsp;Jarry (5 minutes à pied du métro)
-                </p>
-              </address>
-
-              <span class="font-weight-bold"
-                >Horaires (à titre indicatif) :</span
-              >
-              <ul class="no-bullets">
-                <li>Mardi - 14:00 à 20:00</li>
-                <li>Mercredi - 10:00 à 18:00</li>
-                <li>Vendredi - 09:00 à 17:00</li>
-              </ul>
-            </v-card-text>
-            <v-card-actions class="justify-center">
-              <v-btn
-                text
-                class="custom-color-grey black--text pa-sm-7 mb-2 font-weight-bold text-sm-h6 text-subtitle-2"
-                href="https://physiotheraplus.com/contact-villeray/"
-                target="_blank"
-              >
-                Prendre rendez-vous
-              </v-btn>
-            </v-card-actions>
-          </v-card>
-        </v-col>
-
-        <v-col cols="12" sm="6" class="map">
-          <iframe
-            src="https://www.google.com/maps/embed/v1/place?key=AIzaSyCNsDQVC0fSNhGNPsppuAotr38HuU5Hgqk&q=Clinique Physio Théra Plus - Villeray, 8290 R. Saint-Denis #2, Montréal, QC H2P 2G6, Canada"
-            frameborder="0"
-            scrolling="no"
-            width="100%"
-            height="300"
-          >
-          </iframe>
-        </v-col>
-      </v-row>
     </v-card-text>
   </v-card>
 </template>
