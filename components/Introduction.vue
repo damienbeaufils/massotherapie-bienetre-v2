1f5531fea93d66c46e80c4bdfe595d3d7e706c9e
Damien Beaufils <damien.beaufils@gmail.com>
2023-11-07 10:04:37 -0500

    💬 add vacations banner

--- components/Introduction.vue
+++ components/Introduction.vue
@@ -1,17 +1,15 @@
 <template>
   <v-row justify="center">
-    <!--
     <v-col cols="12">
       <v-card rounded>
         <v-card-text
           class="red--text custom-color-pink text-h6 text-sm-h5 text-center font-weight-bold"
         >
-          Fermeture temporaire pour cause de travaux. Réouverture en septembre
+          Fermeture temporaire pour cause de congés. Réouverture mi-décembre
           2023 !
         </v-card-text>
       </v-card>
     </v-col>
-    -->
     <v-col cols="12">
       <v-img
         src="/img/virginie_dang_massage.jpg"
